814c1206248fd0c4efcddab3678fc49c1ebc992e
Octavio Gonzalez-Lugo <ogonzalezl@outlook.com>
2023-02-26 00:18:55 -0600

    Update wavelengths.py

--- wavelengths.py
+++ wavelengths.py
@@ -24,7 +24,4 @@ dataFrame['measurement_precision'] = datasplited[:,7].astype(np.float64)
 dataFrame['measurement_stability'] = datasplited[:,8].astype(np.float64)
 dataFrame['quality'] = datasplited[:,9].astype(np.float64)
 
-wavelengths = dataFrame['wavelength'].unique()
-wbins = [wavelengths[0],wavelengths[485],wavelengths[680],wavelengths[710],wavelengths[820],wavelengths[950],wavelengths[1049],wavelengths[1172],wavelengths[-1]]
-
 dataFrame.to_csv('/home/tavo/Documentos/solarcurrent.csv',index=False)
